3e110247e9d5a0569c0688d4e19f24b5440c81c4
Nicolas Bueno <nicolasbrian202@gmail.com>
2023-08-27 18:43:26 -0300

    Create repositoryProduct

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -1,16 +1,26 @@
 package com.example.crud.controllers;
 
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.crud.domain.Product.Product;
+import com.example.crud.domain.Product.ProductRepository;
+
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository repository;
+
     @GetMapping
-    public ResponseEntity<String> getAllProduct(){
-        return ResponseEntity.ok("deu ok");
+    public ResponseEntity<List<Product>> getAllProduct(){
+        var allProduct = repository.findAll();
+        return ResponseEntity.ok(allProduct);
     }
 }
